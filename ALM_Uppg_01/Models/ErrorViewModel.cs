fadc2da827ea8900be17b499dac8672c17dbf7dc
Michael <ermifo90@gmail.com>
2019-11-12 17:47:50 +0100

    Add project files.

---
+++ ALM_Uppg_01/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ALM_Uppg_01.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
