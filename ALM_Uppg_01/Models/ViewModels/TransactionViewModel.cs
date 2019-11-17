c0218ae8adb2461b7f95e89c979032d1fa2a51e6
Michael Fors <37196376+ErikMichaelFors@users.noreply.github.com>
2019-11-17 17:29:54 +0100

    Transaktions-sidan klar (#2)
    * Transaktions-sidan klar
    
    * changed name of 2 TestMethods
    
    * README change
    
    * Changed Display properties

---
+++ ALM_Uppg_01/Models/ViewModels/TransactionViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ALM_Uppg_01.Models.ViewModels
+{
+    public class TransactionViewModel
+    {
+        public DepositViewModel DepositVM { get; set; }
+        public WithdrawalViewModel WithdrawalVM { get; set; }
+
+        [Required]
+        [Display(Name = "Account number")]
+        public int WithdrawalAccountId { get; set; }
+
+        [Required]
+        [Display(Name = "Amount")]
+
+        public decimal WithdrawalAmount { get; set; }
+
+        public string WithdrawalErrorMessage { get; set; }
+
+        public string WithdrawalSuccessMessage { get; set; }
+
+
+        [Required]
+        [Display(Name = "Account number")]
+
+        public int DepositAccountId { get; set; }
+
+        [Required]
+        [Display(Name = "Amount")]
+
+        public decimal DepositAmount { get; set; }
+
+        public string DepositErrorMessage { get; set; }
+
+        public string DepositSuccessMessage { get; set; }
+
+
+        public TransactionViewModel()
+        {
+            DepositVM = new DepositViewModel();
+            WithdrawalVM = new WithdrawalViewModel();
+        }
+    }
+}
---
+++ Tests/UnitTests.cs
@@ -0,0 +1,230 @@
+using ALM_Uppg_01.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Tests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void Withdrawal_CantWithdrawMoreThanBalance()
+        {
+            // Arrange
+            var customers = new List<Customer>()
+            {
+                new Customer()
+                {
+                    Name = "Hannibal",
+                    Id = 1,
+                    Accounts = new List<Account>()
+                    {
+                        new Account()
+                        {
+                            AccountId = 1,
+                            Balance = 400M
+                        }
+                    }
+                }
+            };
+            BankRepository.AddCustomers(customers);
+            decimal amount = 500M;
+            decimal expected = 400M;
+
+            // Act
+            // customer id, account id, amount
+            var actualAccount = BankRepository.Withdrawal(1, amount);
+
+            // Assert
+            Assert.Equal(expected, actualAccount.Balance, 2);
+        }
+
+        [Fact]
+        public void Withdrawal_CantWidthdrawNegativeAmount()
+        {
+            // Arrange
+            var customers = new List<Customer>()
+            {
+                new Customer()
+                {
+                    Name = "Hannibal",
+                    Id = 2,
+                    Accounts = new List<Account>()
+                    {
+                        new Account()
+                        {
+                            AccountId = 2,
+                            Balance = 600M
+                        }
+                    }
+                }
+            };
+            BankRepository.AddCustomers(customers);
+            decimal amount = -500M;
+            decimal expected = 600M;
+
+            // Act
+            var account = BankRepository.Withdrawal(2, amount);
+
+            // Assert
+            Assert.Equal(expected, account.Balance, 2);
+        }
+        
+        [Fact]
+        public void Withdrawal_CanMakeBasicWidthdrawal()
+        {
+            // Arrange
+            var customers = new List<Customer>()
+            {
+                new Customer()
+                {
+                    Name = "Hannibal",
+                    Id = 3,
+                    Accounts = new List<Account>()
+                    {
+                        new Account()
+                        {
+                            AccountId = 3,
+                            Balance = 600M
+                        }
+                    }
+                }
+            };
+            BankRepository.AddCustomers(customers);
+            decimal amount = 200M;
+            decimal expected = 400M;
+
+            // Act
+            var account = BankRepository.Withdrawal(3, amount);
+
+            // Assert
+            Assert.Equal(expected, account.Balance, 2);
+        }
+
+        [Fact]
+        public void Withdrawal_CantWithdrawFromNonExistingAccount()
+        {
+            // Arrange
+            var customers = new List<Customer>()
+            {
+                new Customer()
+                {
+                    Name = "Hannibal",
+                    Id = 3,
+                    Accounts = new List<Account>()
+                    {
+                        new Account()
+                        {
+                            AccountId = 3,
+                            Balance = 600M
+                        }
+                    }
+                }
+            };
+            BankRepository.AddCustomers(customers);
+            decimal amount = 200M;
+            string expected = BankRepository.CantAccessNonExistingAccount;
+
+            // Act
+            BankRepository.Withdrawal(1, amount);
+
+            // Assert
+            Assert.Equal(expected, BankRepository.ErrorMessage);
+        }
+
+
+        [Fact]
+        public void Deposit_CanMakeBasicDeposit()
+        {
+            // Arrange
+            var customers = new List<Customer>()
+            {
+                new Customer()
+                {
+                    Name = "Hannibal",
+                    Id = 4,
+                    Accounts = new List<Account>()
+                    {
+                        new Account()
+                        {
+                            AccountId = 4,
+                            Balance = 600M
+                        }
+                    }
+                }
+            };
+            BankRepository.AddCustomers(customers);
+            decimal amount = 200M;
+            decimal expected = 800M;
+
+            // Act
+            var account = BankRepository.Deposit(4, amount);
+
+            // Assert
+            Assert.Equal(expected, account.Balance, 2);
+        }
+
+        [Fact]
+        public void Deposit_CantDepositNegativeAmount()
+        {
+            // Arrange
+            var customers = new List<Customer>()
+            {
+                new Customer()
+                {
+                    Name = "Hannibal",
+                    Id = 5,
+                    Accounts = new List<Account>()
+                    {
+                        new Account()
+                        {
+                            AccountId = 5,
+                            Balance = 600M
+                        }
+                    }
+                }
+            };
+            BankRepository.AddCustomers(customers);
+            decimal amount = -200M;
+            decimal expected = 600M;
+
+            // Act
+            var account = BankRepository.Deposit(5, amount);
+
+            // Assert
+            Assert.Equal(expected, account.Balance, 2);
+        }
+
+        [Fact]
+        public void Deposit_CantDepositToNonExistingAccount()
+        {
+            // Arrange
+            var customers = new List<Customer>()
+            {
+                new Customer()
+                {
+                    Name = "Hannibal",
+                    Id = 3,
+                    Accounts = new List<Account>()
+                    {
+                        new Account()
+                        {
+                            AccountId = 3,
+                            Balance = 600M
+                        }
+                    }
+                }
+            };
+            BankRepository.AddCustomers(customers);
+            decimal amount = 200M;
+            string expected = BankRepository.CantAccessNonExistingAccount;
+
+            // Act
+            BankRepository.Deposit(1, amount);
+
+            // Assert
+            Assert.Equal(expected, BankRepository.ErrorMessage);
+        }
+    }
+}
